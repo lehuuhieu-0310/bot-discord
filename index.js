e5e6b5b00cd2913aa7280c230b14cf25659f38fe
lehuuhieu-0310 <01643815296h@gmail.com>
2021-09-12 23:25:25 +0700

    third commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { Client, MessageAttachment } = require('discord.js')
-const client = new Client()
+const { Client, MessageAttachment, Intents } = require('discord.js')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 const axios = require('axios')
 const moment = require('moment')
 require('dotenv').config()
@@ -11,7 +11,7 @@ client.once('ready', () => {
     console.log('ready!')
 })
 
-client.on('message', getImage)
+client.on('messageCreate', getImage)
 
 function getImage(message) {
 
